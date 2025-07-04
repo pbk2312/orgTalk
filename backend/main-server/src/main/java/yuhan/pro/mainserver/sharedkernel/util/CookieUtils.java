fb4dc4142d4e9a69faacd6f0d3601b8fbd4f7c92
pbk2312 <pbk2312@inu.ac.kr>
2025-07-04 11:17:39 +0900

    refactor: https 빼기

--- backend/main-server/src/main/java/yuhan/pro/mainserver/sharedkernel/util/CookieUtils.java
+++ backend/main-server/src/main/java/yuhan/pro/mainserver/sharedkernel/util/CookieUtils.java
@@ -5,31 +5,31 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class CookieUtils {
 
-  private CookieUtils() {
-    // 인스턴스화 방지
-  }
+    private CookieUtils() {
+        // 인스턴스화 방지
+    }
 
-  public static void addCookie(HttpServletResponse response, String name, String value,
-      int maxAge) {
-    Cookie cookie = new Cookie(name, value);
-    cookie.setHttpOnly(true);
-    cookie.setSecure(true);
-    cookie.setPath("/");
-    cookie.setMaxAge(maxAge);
-    response.addCookie(cookie);
-  }
+    public static void addCookie(HttpServletResponse response, String name, String value,
+            int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
 
-  public static void addCsrfCookie(HttpServletResponse response, String name, String value,
-      int maxAge) {
-    Cookie cookie = new Cookie(name, value);
-    cookie.setHttpOnly(false); // JS에서 읽을 수 있도록 false
-    cookie.setSecure(true);
-    cookie.setPath("/");
-    cookie.setMaxAge(maxAge);
-    response.addCookie(cookie);
-  }
+    public static void addCsrfCookie(HttpServletResponse response, String name, String value,
+            int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(false); // JS에서 읽을 수 있도록 false
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
 
-  public static void removeCookie(HttpServletResponse response, String name) {
-    addCookie(response, name, null, 0);
-  }
+    public static void removeCookie(HttpServletResponse response, String name) {
+        addCookie(response, name, null, 0);
+    }
 }
