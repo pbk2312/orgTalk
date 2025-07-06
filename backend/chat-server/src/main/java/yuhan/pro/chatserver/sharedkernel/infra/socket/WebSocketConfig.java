1e8ce8933efdfb944eac380a89aa3db284e67408
pbk2312 <pbk2312@inu.ac.kr>
2025-07-06 10:49:11 +0900

    fix : /api/chat 접두어 문제 해결

--- backend/chat-server/src/main/java/yuhan/pro/chatserver/sharedkernel/infra/socket/WebSocketConfig.java
+++ backend/chat-server/src/main/java/yuhan/pro/chatserver/sharedkernel/infra/socket/WebSocketConfig.java
@@ -13,23 +13,23 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  private final StompHandler stompHandler;
+    private final StompHandler stompHandler;
 
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.setApplicationDestinationPrefixes("/send");
-  }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/send");
+    }
 
-  @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry
-        .addEndpoint("/ws-stomp")
-        .setAllowedOriginPatterns("http://localhost:3000")
-        .withSockJS();
-  }
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/api/chat/ws-stomp")
+                .setAllowedOriginPatterns("http://localhost:3000")
+                .withSockJS();
+    }
 
-  @Override
-  public void configureClientInboundChannel(ChannelRegistration registration) {
-    registration.interceptors(stompHandler);
-  }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
 }
