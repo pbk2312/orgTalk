07c0bddac009a905d8c316215c087ac14805ab4a
pbk2312 <pbk2312@inu.ac.kr>
2025-07-07 21:41:07 +0900

    refactor:  환경 변수 수정

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,8 +1,7 @@
-// src/lib/constants.ts
-
 const origin = window.location.origin;
 
-export const API_BASE_URL = process.env.REACT_APP_MAIN_SERVER_URL || '/api';
-export const CHAT_BASE_URL = process.env.REACT_APP_CHAT_SERVER_URL || '/api/chat';
-export const CHAT_ROOM_BASE_URL = process.env.REACT_APP_CHAT_ROOM_SERVER_URL || '/api/chatroom';
-export const OAUTH_BASE_URL = process.env.REACT_APP_OAUTH_BASE_URL || origin;
+// 값이 없으면 localhost 사용
+export const API_BASE_URL = 'http://localhost:8080/api';
+export const CHAT_BASE_URL =  'http://localhost:8081/api/chat';
+export const CHAT_ROOM_BASE_URL = 'http://localhost:8081/api/chatroom';
+export const OAUTH_BASE_URL = 'http://localhost:8080/oauth';
