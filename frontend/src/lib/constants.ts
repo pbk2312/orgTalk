4f008f9eb78354e0d2948ff3d51e0544323c2a30
pbk2312 <pbk2312@inu.ac.kr>
2025-07-08 23:15:02 +0900

    fix : 경로 문제 해결

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -2,6 +2,6 @@ const origin = window.location.origin;
 
 
 export const API_BASE_URL = 'http://localhost:8080/api';
-export const CHAT_BASE_URL =  'http://localhost:8081/api/chat';
+export const CHAT_BASE_URL = 'http://localhost:8081/api/chat';
 export const CHAT_ROOM_BASE_URL = 'http://localhost:8081/api/chatroom';
-export const OAUTH_BASE_URL = 'http://localhost:8080/oauth';
+export const OAUTH_BASE_URL = 'http://localhost:8080';
