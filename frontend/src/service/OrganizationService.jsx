2f4dad9778c3f1464117cae6ac303f8ccfb217bc
pbk2312 <pbk2312@inu.ac.kr>
2025-08-03 19:40:59 +0900

    fix: url 변경

--- frontend/src/service/OrganizationService.jsx
+++ frontend/src/service/OrganizationService.jsx
@@ -2,7 +2,7 @@ import api from '../lib/axios.ts';
 
 export async function getOrganizationInfo(organizationId) {
   try {
-    const response = await api.get(`/organization/${organizationId}`);
+    const response = await api.get(`/api/organization/${organizationId}`);
     return response.data;
   } catch (error) {
     console.error('Failed to fetch organization info:', error);
