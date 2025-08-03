078ff8b44ebfb5d97ed7e115dccb20271cbd3ab6
pbk2312 <pbk2312@inu.ac.kr>
2025-08-03 21:24:04 +0900

    fix: url 변

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -68,7 +68,7 @@ const LoginPage = () => {
   const handleGitHubLogin = () => {
   setIsLoading(true);
   setTimeout(() => {
-    window.location.href = `${OAUTH_BASE_URL}/oauth2/authorization/github`;
+    window.location.href = `${OAUTH_BASE_URL}/login/oauth2/authorization/github`;
   }, 500);
 };
 
