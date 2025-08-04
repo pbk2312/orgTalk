8e40ddb4b128110fa529bb79cd819e0c3bdf2d1b
pbk2312 <pbk2312@inu.ac.kr>
2025-08-04 15:05:56 +0900

    fix: rabbitMQ 목 처리

--- backend/chat-server/src/test/java/yuhan/pro/chatserver/ChatServerApplicationTests.java
+++ backend/chat-server/src/test/java/yuhan/pro/chatserver/ChatServerApplicationTests.java
@@ -1,13 +1,19 @@
 package yuhan.pro.chatserver;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChatServerApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
